2a10a52deac9c08594020d97d67b1126ed28b879
LuisZeledon88 <luiscarloszeledonmolina@gmail.com>
2025-04-07 15:36:47 -0600

    Add files via upload

---
+++ team-app-main/src/components/MembersTable.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="place-self-center flex flex-col gap-y-3">
+      <table class="table-fixed border-spacing-2 border-separate">
+        <thead>
+          <tr>
+            <th class="text-left px-6 py-2">Name</th>
+            <th class="text-left px-6 py-2">Email</th>
+            <th class="text-left px-6 py-2">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <MemberRow v-for="(member, index) in members" :key="index" :member="member" />
+        </tbody>
+      </table>
+      <p class="text-right text-gray-600 italic">There are no remaining team spots. Upgrade to add more.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import MemberRow from './MemberRow.vue'
+  defineProps(['members'])
+  </script>
+  
---
+++ team-app-main/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import MembersTable from '@/components/MembersTable.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import membersData from '@/data/members.json'
+
+const members = ref([])
+
+onMounted(() => {
+  members.value = membersData
+})
+</script>
+
+<template>
+  <HeaderComponent :totalMembers="members.length" />
+  <MembersTable :members="members" />
+  <FooterComponent :totalMembers="members.length" />
+</template>
